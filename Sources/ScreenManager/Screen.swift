158a0579a372ecdefe77f288165562dfff3c36ab
Mathieu <mathieu.perroud@dreem.com>
2023-03-03 12:14:08 +0100

    [ScreenManager/Screen] Removed `arguments` and `PresentationMode`

--- Sources/ScreenManager/Screen.swift
+++ Sources/ScreenManager/Screen.swift
@@ -3,24 +3,17 @@ import SwiftUI
 internal class Screen: UIHostingController<AnyView>, Identifiable {
     let id: UUID = UUID()
     let name: String
-    let arguments: [String: Any]
-    var onNavigateTo: ([String: Any]) -> Void = { _ in }
-    
+    var onNavigateTo: ([String : Any]) -> Void = { _ in }
+
     init(
         name: String,
-        arguments: [String: Any] = [:],
         view: some View
     ) {
         self.name = name
-        self.arguments = arguments
-        super.init(rootView: AnyView(view.background(ScreenTheme.backgroundColor.edgesIgnoringSafeArea(.all))))
+        super.init(rootView: AnyView(view))
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-}
-
-internal enum PresentationMode {
-    case sheet, fullScreen
 }

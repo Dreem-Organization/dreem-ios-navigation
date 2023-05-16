445b4aeef045c7b5e35dd228000dde1841a69fa7
VincentDreem <vincent.lemonnier@dreem.com>
2023-05-16 09:59:31 +0200

    [DreemNav] Changed TransitionStyle values

--- Sources/TransitionStyle.swift
+++ Sources/TransitionStyle.swift
@@ -1,3 +1,20 @@
+/// Transition styles available when pushing screens.
+///
+/// Use this options when calling method `push`.
+/// ```
+///   navigationController.push(screenName: "ScreenB", transition: .coverHorizontal)
+/// ```
 public enum TransitionStyle {
-    case coverVertical, crossDissolve, coverHorizontal, sheet, dialog
+    /// The transition between two screens will be instant, no animation will be applied.
+    case none
+    /// The transition will cover the entire screen and a cross-disolve animation will be executed.
+    case coverFullscreen
+    /// The transition will cover the entire screen and a cross-disolve animation will be executed. The previous screen will be kept in the view hiearchy beneath the newly pushed screen.
+    case coverOverFullscreen
+    /// The transition will cover the entire screen from the bottom to the top on pushing, and reverse on popping.
+    case coverVertical
+    /// The transition will cover the entire screen from the right to the left on pushing, and reverse on popping.
+    case coverHorizontal
+    /// The transition will partially covers the underlying content.
+    case sheet
 }
